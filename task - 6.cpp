b4293a38f0be1bf9860fe70b279ce91ff052bd84
DiabloXV <105218823+DiabloXV@users.noreply.github.com>
2022-05-09 15:32:44 +0200

    Add files via upload

---
+++ task - 6.cpp
@@ -0,0 +1,18 @@
+// task - 6.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+//INITIAL PHASE
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    bool seen;
+    bool ChatIsOpened;//ChatIsOpened is variable came from task 2
+
+    if (ChatIsopened == true)
+    {
+        return seen = true;
+    }
+    else
+        return seen = false;
+}
